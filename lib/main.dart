04cc3b8d24cff4763749307617cab9248fe0a8f2
Bakhodur K <bakhbk@gmail.com>
2020-06-15 13:55:53 +0300

    done challenge app https://www.appbrewery.co/courses/851555/lectures/15448478

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue.shade800,
+          ),
+          body: BallPage(),
+        ),
       ),
     );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: onChangeBallImage,
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onChangeBallImage() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+}
